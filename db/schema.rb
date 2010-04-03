df9e0aa9d3165568c1eadb16564412f1cf99e9dc
Mahesh Murthy <mahesh@mmurthy.com>
2010-04-03 16:52:21 -0600

    created tag model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100326003941) do
+ActiveRecord::Schema.define(:version => 20100403224907) do
 
   create_table "bars", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,12 @@ ActiveRecord::Schema.define(:version => 20100326003941) do
     t.integer  "userid"
     t.string   "phone"
     t.integer  "bar_id"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "keyword"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
