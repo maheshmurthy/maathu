08f54e76da92352cae89f68045fbe68058f6c314
Mahesh Murthy <mahesh@mmurthy.com>
2010-04-16 07:38:10 -0500

    views cleanup, added sticky footer

--- lib/specials_util.rb
+++ lib/specials_util.rb
@@ -1,5 +1,5 @@
 module SpecialsUtil
   def getSpecialsOfDay(day)
-    Special.all(:select => "specials.id, #{day} as day_special, bar_id, name", :joins => :bar)
+    Special.all(:select => "specials.id, #{day} as day_special, bar_id, name", :joins => :bar, :conditions => "#{day} not like ''")
   end
 end
